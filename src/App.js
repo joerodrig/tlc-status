753e36dc0ddafe4f912ae04c29da2cea2b47d3f2
Joseph <joerodrig3@gmail.com>
2017-10-26 00:30:17 -0400

    Initial app page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,87 @@
+import "whatwg-fetch";
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TLCApplication from "./TLCApplication";
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appNumber: null,
+      submitted: false,
+      error: null,
+      application: {
+        // app_date: "2017-08-14T00:00:00.000",
+        // app_no: "5811854",
+        // defensive_driving: "Complete",
+        // driver_exam: "Needed",
+        // drug_test: "Needed",
+        // fru_interview_scheduled: "Not Applicable",
+        // lastupdate: "2017-10-25T18:00:25.000",
+        // medical_clearance_form: "Complete",
+        // other_requirements: "Fingerprints needed",
+        // status: "Incomplete",
+        // type: "HDR",
+        // wav_course: "Complete",
+      }
+    };
+  }
+
+  _taxiInfo = () => {
+    const baseUrl = "https://data.cityofnewyork.us/resource/xtra-f75s.json";
+    const appNumberFilterParam = `app_no=${this.state.appNumber}`;
+
+    return fetch(`${baseUrl}?${appNumberFilterParam}`, {
+    method: "GET",
+    body: {
+      "$limit" : 2,
+      //"$$app_token" : "not necessary yet :)"
+    }
+  })
+    .then(response =>
+      response.json().then(json => response.ok ? json : Promise.reject(json))
+    )
+    .then(response => {
+      // If the response returns an empty array, the application wasn't found
+      if (response.length === 0) {
+        this.setState(() => ({ error: { code: "404" }, submitted: true }))
+      }
+
+      // Set application from the response. The response returns an array,
+      // but since we're filtering by App number and app numbers are unique,
+      // we just take the first response
+      this.setState(() => ({ application: response[0], submitted: true }))
+    })
+    .catch(response => {
+      this.setState(() => ({ error: response, submitted: true }))
+    });
+  }
+
+  _updateAppNumber = (val) => (
+    this.setState(() => ({ appNumber: val }))
+  )
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-form">
+          <h3>Application Number</h3>
+          <input
+            type="number"
+            defaultValue={this.state.appNumber}
+            onChange={(e) => this._updateAppNumber(e.target.value)}
+          />
+          <button onClick={() => this._taxiInfo()} type="submit">Search</button>
+        </div>
+
+        {this.state.submitted &&
+          <TLCApplication
+            application={this.state.application}
+            error={this.state.error}
+          />
+        }
       </div>
     );
   }
---
+++ src/TLCApplication.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+const errorType = (error) => {
+  if (error.code === "query.compiler.malformed") {
+    return "The application number entered is invalid";
+  } else if (error.code === "404") {
+    return "We were unable to find your application";
+  }
+}
+
+const completionCount = (application) => {
+  const requirementKeys = [
+    "defensive_driving",
+    "driver_exam",
+    "drug_test",
+    "fru_interview_scheduled",
+    "wav_course",
+    "other requirements",
+  ];
+  const totalRequirementsCount = requirementKeys.filter((k) => application[k] !== "Not Applicable").length
+  const completedRequirementsCount = requirementKeys.filter((k) => application[k] === "Complete").length
+  return { completed: completedRequirementsCount, total: totalRequirementsCount };
+};
+
+const TLCApplication = ({application, error}) => {
+  if (error) {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+  return (
+    <div className="tlc-application-container">
+      <div className="tlc-application-header">
+        <p className="tlc-application-type">{application.type} Application for {application.app_no}</p>
+        <p className="tlc-application-status">{application.status}</p>
+        <div className="tlc-application-times">
+          <span>Applied on {new Date(application.app_date).toISOString().slice(0,10)}</span>
+          <span>Last Updated on {new Date(application.lastupdate).toISOString().slice(0,10)}</span>
+        </div>
+      </div>
+
+
+      <div className="tlc-application-requirements">
+        <h2>Requirements</h2>
+        <span><i>({completionCount(application).completed}/{completionCount(application).total}) complete</i></span>
+        <div className="tlc-application-requirement">
+          <p>Defensive driving {application.defensive_driving}</p>
+        </div>
+        <div className="tlc-application-requirement">
+          <p>Driver exam {application.driver_exam}</p>
+        </div>
+        <div className="tlc-application-requirement">
+          <div>Drug test {application.drug_test}</div>
+        </div>
+        <div className="tlc-application-requirement">
+          <p>Wav course {application.wav_course}</p>
+          {/* Uber wav training docs: https://www.uber.com/drive/new-york/get-a-license/wav-training/ */}
+        </div>
+        <div className="tlc-application-requirement">
+          <p>Defensive driving {application.defensive_driving}</p>
+        </div>
+
+
+
+        <div>Medical clearance form {application.medical_clearance_form}</div>
+        <div>Other requirements {application.other_requirements}</div>
+
+        <div>fru interview scheduled {application.fru_interview_scheduled}</div>
+      </div>
+    </div>
+  );
+}
+
+export default TLCApplication;
