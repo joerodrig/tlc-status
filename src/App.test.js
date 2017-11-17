f84e9f49afd169d30de961a7e95e5688b836ffc9
Joseph <joerodrig3@gmail.com>
2017-11-17 01:14:24 -0500

    Testing App component rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import ApplicationRequestForm from "./ApplicationRequestForm";
+import TLCApplication from "./TLCApplication";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe("App", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should render without throwing an error', () => {
+    expect(wrapper.find(".App")).toHaveLength(1);
+  });
+
+  describe("Application submitted", () => {
+    describe("errors exist", () => {
+      beforeEach(() => {
+        wrapper.setState({
+          submitted: true,
+          error: "uh oh!",
+        });
+      });
+
+      it("should display the application request form component", () => {
+        expect(wrapper.find(ApplicationRequestForm)).toHaveLength(1);
+      });
+
+      it("should display the TLCApplication component", () => {
+        expect(wrapper.find(TLCApplication)).toHaveLength(1);
+      });
+    });
+
+    describe("no errors exist", () => {
+      beforeEach(() => {
+        wrapper.setState({
+          submitted: true,
+          error: null,
+        });
+      });
+
+      it("should NOT display the application request form component", () => {
+        expect(wrapper.find(ApplicationRequestForm)).toHaveLength(0);
+      });
+
+      it("should display the TLCApplication component", () => {
+        expect(wrapper.find(TLCApplication)).toHaveLength(1);
+      });
+    });
+  });
+
+  describe("Application not submitted", () => {
+    it("should display the application request form component", () => {
+      expect(wrapper.find(ApplicationRequestForm)).toHaveLength(1);
+    });
+
+    it("should NOT display the TLCApplication component", () => {
+      expect(wrapper.find(TLCApplication)).toHaveLength(0);
+    });
+  });
 });
