25dbe6eef75efcd5d18a414a6384d4261deee47b
joerodrig <joerodrig3@gmail.com>
2018-05-20 21:55:28 -0400

    Fix props

--- src/TLCApplicationFooter.js
+++ src/TLCApplicationFooter.js
@@ -4,13 +4,13 @@ const formatDate = (date) => (
   new Date(date).toISOString().slice(0,10)
 );
 
-const TLCApplicationFooter = ({app_date, lastupdate}) => (
+const TLCApplicationFooter = ({appDate, lastUpdate}) => (
   <div className="tlc-application-footer">
     <span className="tlc-application-footer--applied">
-      Applied on {formatDate(app_date)}
+      Applied on {formatDate(appDate)}
     </span>
     <span className="tlc-application-footer--updated">
-      Updated on {formatDate(lastupdate)}
+      Updated on {formatDate(lastUpdate)}
     </span>
   </div>
 );
